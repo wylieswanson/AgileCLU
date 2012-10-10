cd1afb6247ae82aac071c451c9d848d99ff26847
Wylie Swanson <wylie@pingzero.net>
2012-10-09 21:11:00 -0700

    clean-up for basic working release before phase 2

--- CHANGES.md
+++
@@ -1,5 +0,0 @@
-CHANGES
-=======
-
-* 0.3.1 - configuration file format change
-* 0.3.0 - major overhaul, re-read README!  
--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here,"README")).read()
 
 setup(  
 	name="AgileCLU",
-	version="0.3.1",
+	version="0.3.2",
    install_requires=['poster','progressbar','pydes','jsonrpclib'],
+	packages=['AgileCLU'],
+	package_data={'': ['LICENSE','README.md']},
+	include_package_data=True,
+	scripts=['bin/agilels', 'bin/agilefetch', 'bin/agilemkdir', 'bin/agilepost', 'bin/agileprofile', 'bin/agilerm'],
 
 	description="Agile Command Line Utilities",
-	long_description="""
-This package uses the Limelight Networks Agile Storage APIs to manage objects in the Agile Cloud.
-""",
-
+	long_description=readme,
 	author="Wylie Swanson",
 	author_email="wylie@pingzero.net",
 	url="http://www.pingzero.net",
-	download_url = "http://www.pingzero.net/downloads/AgileCLU-0.3.0.tar.gz",
+	download_url = "https://github.com/wylieswanson/AgileCLU/raw/master/dist/AgileCLU-0.3.2.tar.gz",
+
+	platforms = ("Any",),
+	keywords = ("agile", "storage", "limelight", "cloud", "object" ),
 
 	classifiers = [	'Development Status :: 4 - Beta',
 							'License :: OSI Approved :: BSD License',
@@ -22,11 +30,5 @@ This package uses the Limelight Networks Agile Storage APIs to manage objects in
 							'Intended Audience :: End Users/Desktop',
 							'Environment :: Console',
 							'Topic :: Utilities',
-							],
-
-	platforms = ("Any",),
-	keywords = ("agile", "storage", "limelight", "cloud", "object" ),
-
-	packages=['AgileCLU'],
-	scripts=['bin/agilels', 'bin/agilefetch', 'bin/agilemkdir', 'bin/agilepost', 'bin/agileprofile', 'bin/agilerm'],
+							]
 	)
